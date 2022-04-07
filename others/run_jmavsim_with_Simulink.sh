37ee9850ee803906def1595921c629e00aa05639
liuwhale <liuwhale@Cetacea>
2022-04-07 22:43:53 +0800

    [传说中的羊咩咩] Add run_jmavsim_with_Simulink.sh.

---
+++ others/run_jmavsim_with_Simulink.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#脚本的参数需要根据自己的情况替换
+set -e
+
+/cygdrive/d/PX4/home/Firmware/Tools/sitl_run_no_lockstep.sh /cygdrive/c/Users/liuwhale/AppData/Local/Temp/PX4_CO~1/PX4HOS~1/px4.exe none jmavsim none /cygdrive/d/PX4/home/Firmware /cygdrive/d/PX4/home/Firmware/build/px4_sitl_default
+

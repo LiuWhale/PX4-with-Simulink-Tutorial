b97fc5d6b170cc6ed26b4e6d6b8fadc1dd090704
liuwhale <liuwhale@Cetacea>
2022-04-07 22:37:49 +0800

    [传说中的羊咩咩] add UAV height control model.

---
+++ utilities/launchQuadcopterAltitudeControlModel.m
@@ -0,0 +1,9 @@
+function launchSimulinkNumberFive
+%
+
+%   Copyright 2021 The MathWorks, Inc.
+
+    project = simulinkproject;
+    projectRoot = project.RootFolder;
+    open_system(fullfile(projectRoot,'models','No5_QuadcopterAltitudeControl.slx'));
+end
